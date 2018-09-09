365c76dd1b8981ea461cb27298ff6222b468d930
subham <subham.bhattacharjee1@gmail.com>
2018-09-10 01:01:58 +0530

    Working copy without styling

--- src/App.js
+++ src/App.js
@@ -8,19 +8,56 @@ const API_KEY = "5ee584d99e185a3b96892852b7eb5f95";
 // uhttp://api.openweathermap.org/data/2.5/weather?q=Malda,India&appid=5ee584d99e185a3b96892852b7eb5f95&units=metric
 
 class App extends React.Component {
+	state = {
+		temperature: undefined,
+		city: undefined,
+		country: undefined,
+		humidity: undefined,
+		description: undefined,
+		error: undefined
+	}
 
-	getWeather = async () => {
-		const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Malda,India&appid=${API_KEY}&units=metric');
-		const data = await api_call.json();
-		console.log(data);
+	getWeather = async (e) => {
+		e.preventDefault();
+		const city = e.target.elements.city.value;
+		const country = e.target.elements.country.value;
+		if (city && country) {
+			const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+			const data = await api_call.json();
+			console.log(data);
+			this.setState({
+				temperature: data.main.temp,
+				city: data.name,
+				country: data.sys.country,
+				humidity: data.main.humidity,
+				description: data.weather[0].description,
+				error: ""
+			})
+		} else {
+			this.setState({
+				temperature: undefined,
+				city: undefined,
+				country: undefined,
+				humidity: undefined,
+				description: undefined,
+				error: "Please enter the values"
+			})
+		}
 	}
 
 	render() {
 		return (
 			<div>
 				<Titles />
-				<Form />
-				<Weather />
+				<Form getWeather={this.getWeather} />
+				<Weather
+					temperature={this.state.temperature}
+					city={this.state.city}
+					country={this.state.country}
+					humidity={this.state.humidity}
+					description={this.state.description}
+					error={this.state.error}
+				/>
 			</div>
 		);
 	}
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 
-class Title extends React.Component {
-	render() {
-		return (
-			<div>
-			<h1>Weather Finder</h1>
-			<p>Find out Temperature, conditions and more...</p>
-			</div>
-		);
-	}
-}
+const Title = props => (
+	<div>
+		<h1>Weather Finder</h1>
+		<p>Find out Temperature, conditions and more...</p>
+	</div>
+);
 
 export default Title;
