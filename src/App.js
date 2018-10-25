a6e779839bd37ca15da9cafaec5d752fd0762560
Subham Bhattacharjee <subham.bhattacharjee1@gmail.com>
2018-10-25 14:42:35 +0530

    removed line

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Form from "./components/Form";
 import Weather from "./components/Weather";
 
 const API_KEY = "5ee584d99e185a3b96892852b7eb5f95";
-// uhttp://api.openweathermap.org/data/2.5/weather?q=Malda,India&appid=5ee584d99e185a3b96892852b7eb5f95&units=metric
 
 class App extends React.Component {
 	state = {
